b82886c363b3f9bb99ad319094d7b7ed41a40e9a
CЯƶ1_Ðκ <326265648@qq.com>
2022-04-14 13:27:51 +0800

    version update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: MelonInfo(typeof(FC_AP.Main), "FC AP indicator", "1.0.0", "lxy")]
+[assembly: MelonInfo(typeof(FC_AP.Main), "FC AP indicator", "1.0.1", "lxy")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")] 
